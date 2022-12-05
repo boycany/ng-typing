d5dd12d6537f404b7c82861086c7c555f644c0e1
Red Chien <redchien@viridiasolutions.com>
2022-12-05 17:33:03 +0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { getLocaleCurrencySymbol } from '@angular/common';
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-typing';
+  randomText = lorem.sentence();
+  inputText = '';
+
+  onInputChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    // console.log(target.value);
+    this.inputText = target.value;
+  }
+
+  compare(randomLetter: string, inputTextLetter: string) {
+    if (!inputTextLetter) {
+      return 'pending';
+    }
+    return randomLetter === inputTextLetter ? 'correct' : 'incorrect';
+  }
 }
